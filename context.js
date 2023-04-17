cc543b45fd545551439009cf932543cef7c880b3
Al-kodach <alkodach351@gmail.com>
2023-04-17 17:20:01 -0400

    test-complete

--- context.js
+++ context.js
@@ -15,23 +15,24 @@ class Fish {
 const nemo = new Fish('Nemo');
 
 /********************************* Scenario 1 *********************************/
-// eat(); // ?
+ eat(); // undefined eats fish food
 
 
 /********************************* Scenario 2 *********************************/
 // nemo.eat = eat;
-// nemo.eat(); // ?
-
+// nemo.eat(); // nemo eats fish food
+// Type error,   nemo.eat is not a function
 
 /********************************* Scenario 3 *********************************/
 // nemo.eat = eat;
-// eat(); // ?
+eat(); // undefined eats fish food
 
 
 /********************************* Scenario 4 *********************************/
-// nemo.swim(); // ?
+nemo.swim(); // nemo swimming in the water
 
 
 /********************************* Scenario 5 *********************************/
 // const swim = nemo.swim;
-// swim(); // ?
+swim(); // can not invoke on
+// refrence error, swim is not defined
